495b362b1b6b2df88d74eb6b7664454ccb3a9944
Laiane <91036815+Laianekarla@users.noreply.github.com>
2021-12-03 11:54:25 -0300

    @laianekarla [Task: CREATE usermenu component]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { UsermenuComponent } from './usermenu/usermenu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    UsermenuComponent
   ],
   imports: [
     BrowserModule,
