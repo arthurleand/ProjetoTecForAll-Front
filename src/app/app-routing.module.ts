5110445a1a56d5df6001069ba952b1ba1de44762
Gabi.Muniz <mariliamuniz2001@gmail.com>
2021-12-03 15:39:45 -0300

    @GabiMuniz1 [Task: CREATE routes]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { AboutComponent } from './about/about.component';
+import { StartComponent } from './start/start.component';
 
 const routes: Routes = [
-  
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 
+  { path: 'home', component: StartComponent },
+  { path: 'sobre', component: AboutComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
