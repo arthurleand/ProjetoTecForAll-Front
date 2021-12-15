f248bde22364d97a0846b6041ecf5b826c1a3285
Arthur Leandro <arthurdocsgo@gmail.com>
2021-12-14 22:00:53 -0300

    @arthurleand [Task: CREATE METHODS getByIdUSer AND updateUser IN auth.service]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserLoginDTO } from '../model/UserLoginDTO';
 import { environment } from 'src/environments/environment.prod';
 import { UserRegistrationDTO } from '../model/UserRegistrationDTO';
+import { UserModel } from '../model/UserModel';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,15 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  token={
+    headers: new HttpHeaders().set('Authorization',environment.token)
+  }
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+  }
+    
     login(userLoginDTO : UserLoginDTO) : Observable<UserLoginDTO>{
       return this.http.post<UserLoginDTO>('http://localhost:8080/user/login', userLoginDTO)
     }
@@ -21,6 +31,15 @@ export class AuthService {
     register(userRegistrationDTO: UserRegistrationDTO): Observable<UserRegistrationDTO>{
       return this.http.post<UserRegistrationDTO>('http://localhost:8080/user/register',userRegistrationDTO)
     }
+
+    getByIdUser(id: number): Observable<UserModel>{
+      return this.http.get<UserModel>(`'http://localhost:8080/user/${id}`, this.token)
+    }
+
+    updateUser(user: UserModel): Observable<UserModel>{
+      return this.http.put<UserModel>('http://localhost:8080/user/update', this.token)
+    }
+
     logado(){
 
       let ok: boolean = false
